38b2f83e13e6f22b4a049d9c6a8db78d6169e642
ShreyashiKirti01 <95759996+ShreyashiKirti01@users.noreply.github.com>
2022-05-02 19:11:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
-Webcam.set({
-width:350,
-height:300,
-image_format:"jpg",
-jpg_quality:100
+function startClassification(){
+navigator.mediaDevices.getUserMedia({
+audio:true
 });
+classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/W7k5lFMK4/model.json", modelLoaded);
 
-Webcam.attach("#camera");
-
-function take_snapshot(){
-Webcam.snap(function(data_uri){
-document.getElementById("result").innerHTML= "<img id='captured_img' src='"+data_uri+"'>";
-
-});
 }
-
-classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/-0ePWoClj/model.json", modelLoaded);
 
 function modelLoaded(){
-console.log("Model is loaded");
-}
-
-function check(){
-img = document.getElementById("captured_img");
-classifier.classify(img, gotResults);
+console.log("model is loaded");
+classifier.classify(gotResults);
 }
 
 function gotResults(error, results){
 if(error){
-console.log("error");
+console.log(error);
 }
 
-else{
+else(results)
 console.log(results);
-document.getElementById("result_object_name").innerHTML= results[0].label;
-document.getElementById("result_object_accuracy").innerHTML= results[0].confidence.toFixed(2);
-}
+
 }
